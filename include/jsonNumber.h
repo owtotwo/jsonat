8f5708e459b3aeb3db690af5446da91798bf9877
owtotwo <owtotwo@163.com>
2016-05-18 16:33:48 +0800

    toString

--- include/jsonNumber.h
+++ include/jsonNumber.h
@@ -16,29 +16,48 @@ namespace jsonat {
 typedef double Number;
 
 inline bool isInteger(const Number& pt) {
-	return modf(double(pt), static_cast<double*>(nullptr)) == double(0);
+	double tmp;
+	return modf(double(pt), &tmp) == double(0);
 }
 
 inline void toString(std::ostream& os, const Number& pt) {
+	
 	// if is integer, then convert to long long and output
 	if (isInteger(pt)) {
-		
-		os << static_cast<long long>(pt);
+		if (pt >= 1e16) {
+			os.precision(16);
+			os << std::defaultfloat << double(pt);
+		} else {
+			os.precision(0);
+			os << std::fixed << std::showpoint << std::noshowpoint << double(pt);
+		}
 		
 	} else {
 		
-		std::stringstream ss;
-		std::string str;
 		
-		ss.precision(15);
-		ss << std::fixed << pt;
-		ss >> str;
 		
-		size_t pos = str.find_last_not_of('0');
-		assert(pos != std::string::npos);
-		if (pos + 1 < str.length()) str.erase(pos + 1); // has extra trailing zeros
+		if (pt < 1e-6) {
+			
+			os.precision(16);
+			os << std::defaultfloat << double(pt);
+			
+		} else {
+			
+			std::stringstream ss;
+			std::string str;
+			
+			ss.precision(13);
+			ss << std::fixed << double(pt);
+			ss >> str;
+			
+			size_t pos = str.find_last_not_of('0');
+			assert(pos != std::string::npos);
+			if (pos + 1 < str.length()) str.erase(pos + 1); // has extra trailing zeros
+			
+			os << str;
+		}
 		
-		os << str;
+		
 	}
 }
 
